298024f845b0662c56186b77976436e4d9ed828d
Sanjay M <arjunsanjay0@gmail.com>
2022-07-03 01:59:33 +0530

    created casual to formal transfer func

--- grammarify-api/transfer.py
+++ grammarify-api/transfer.py
@@ -14,8 +14,48 @@ ctf_model = AutoModelForSeq2SeqLM.from_pretrained("sanjay-m1/informal-to-formal"
 print("[Transfer] - Informal to Formal model loaded.")
 
 grammar_tokenizer = T5Tokenizer.from_pretrained('sanjay-m1/grammar-corrector-v2', cache_dir="models/")
-grammar_model = T5ForConditionalGeneration.from_pretrained('sanjay-m1/grammar-corrector-v2', cache_dir="models/")\
+grammar_model = T5ForConditionalGeneration.from_pretrained('sanjay-m1/grammar-corrector-v2', cache_dir="models/") \
     .to(torch_device)
 print("[Transfer] - Grammar Corrector model loaded.")
 
 model_loaded = True
+
+
+def transfer(input_sentence, style, quality_filter=0.95):
+    if style == 0:
+        output_sentence = _casual_to_formal(input_sentence, torch_device, quality_filter)
+        return output_sentence
+    # if style == 1:
+    #     output_sentence = _correct_grammar(input_sentence)
+    #     return output_sentence
+
+    else:
+        print("Models aren't loaded for this style, please use the right style during init.")
+
+
+def _casual_to_formal(input_sentence, device, quality_filter, max_candidates=5):
+    ctf_prefix = ""
+    src_sentence = input_sentence
+    input_sentence = ctf_prefix + input_sentence
+    input_ids = ctf_tokenizer.encode(input_sentence, return_tensors='pt')
+    input_ids = input_ids.to(device)
+
+    predictions = ctf_model.generate(
+        input_ids,
+        do_sample=True,
+        max_length=32,
+        top_k=50,
+        top_p=0.95,
+        early_stopping=True,
+        num_return_sequences=max_candidates)
+
+    gen_sentences = set()
+    for prediction in predictions:
+        gen_sentences.add(ctf_tokenizer.decode(prediction, skip_special_tokens=True).strip())
+
+    adequacy_scored_phrases = adequacy.score(src_sentence, list(gen_sentences), quality_filter, device)
+    ranked_sentences = sorted(adequacy_scored_phrases.items(), key=lambda x: x[1], reverse=True)
+    if len(ranked_sentences) > 0:
+        return ranked_sentences[0][0]
+    else:
+        return None
